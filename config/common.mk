2ee99fe5a0aa8fbfa3c2416d08355bfe0bb121f9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    Port face unlock feature
    Build face unlock for arm64 devices
    
    Also add a flag to opt-out
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I859fedca064c32252372ee8f528efc29ac5987d4

--- config/common.mk
+++ config/common.mk
@@ -255,3 +255,15 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/colt/overlay
 
 # Enable ccache
 USE_CCACHE := true
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifeq ($(TARGET_GAPPS_ARCH),arm64)
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
